86669f02a3850981c211cf9287cdfd47bba7640b
unknown <ayushigahoi171@gmail.com>
2025-04-08 10:45:51 +0530

    java

---
+++ datatype.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class datatype {
+    public static void main(String[]args){
+//       int a=5;
+//       byte b=(byte)a;
+//       System.out.println(b);
+//       byte c= 7;
+//       int m=c;
+//       System.out.println(m);
+//       double d=45.2;
+//       short s= (short)d;
+//       System.out.println(s);
+//       float f=50.5f;
+//       double j= f;
+//       System.out.println(j);
+//       short q=38;
+//       double l=(double)q;
+//       System.out.println(l);
+//       int h=67;
+//       double p=(double)h;
+//       System.out.println(p);
+//       short v=27;
+//       long k=(long)f;
+//       System.out.println(k);
+//       char x='h';
+//       int z=(int)x;
+//       System.out.println(z);
+
+//Q5.
+// int a='A';
+// System.out.println(a);
+
+//Q9.
+// int x=5;
+// double y=10;
+// System.out.println(x+y);
+
+//Q6.swap two numbers using temporary variable
+// int a=5;
+// int b=4;
+
+//Q11
+// String name ="Ayushi";
+// int age=20;
+// System.out.println("My name is"+name+"and I am "+age+"old");
+
+//Q7. swap two numbers without using third variable.
+// int a=10;
+// int b=20;
+// a = a+b;
+// b = a-b;
+// a = a-b;
+// System.out.println(a);
+// System.out.println(b);
+
+//sum of num
+// int num=1234;
+// int sum=0;
+// int n;
+// while(n>0){
+//   int temp= num%10;
+//   sum+=temp;
+//   num=num%10;
+// }
+// System.out.println(sum);
+
+
+//Q6. swap two numbers using third variable
+int a=10;
+int b=20;
+int temp;
+temp =a;
+a=b;
+b=temp;
+System.out.println(a);
+System.out.println(b);
+
+
+
+
+
+
+      
+  }
+    
+}
+
---
+++ inputbasics.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class inputbasics {
+public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    String a =sc.next();
+    System.out.println(a);
+
+//     String a=sc.nextLine();
+//     System.out.println(a);
+
+// int number =sc.nextInt();
+// System.out.println(number);
+
+
+
+ }
+}
