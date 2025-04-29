48f12ee13ef995e6cdb6aa9452ca740e1ba5f58b
Ayushi Gahoi <ayushigahoi171@gmail.com>
2025-04-29 15:14:57 +0530

    Add files via upload

---
+++ arearec.java
@@ -0,0 +1,13 @@
+ class arearec {
+    int length=12;
+    int breath=2;
+    int area=length*breath;
+    void arearectangle(){
+        System.out.println("area of rectangle:"+area);
+    }
+    public static void main(String[] args) {
+        arearec obj=new arearec();
+        obj.arearectangle();
+    }
+    }
+
